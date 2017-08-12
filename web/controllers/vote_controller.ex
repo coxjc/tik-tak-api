fc5166b183a58c945b6c227306ef09f288c8a7e3
Colby Cox <coxjc@seas.upenn.edu>
2017-08-12 00:01:31 -0400

    SUB: Vote refactor MSG: Refactor vote controller method.

--- web/controllers/vote_controller.ex
+++ web/controllers/vote_controller.ex
@@ -8,38 +8,38 @@ defmodule Api.VoteController do
 
   def vote(conn, %{"post_id" => post_id, "score" => score}) do
     user = conn.assigns.user
-    user_id = user.id
-    post = Repo.get(Post, 1)
-    cur_vote = from(v in Vote, where: v.user_id == ^user_id and v.post_id == ^post_id) |> Repo.one
-    if !cur_vote do
-      if post && post.visible do
-        changeset = Vote.changeset(%Vote{}, %{user: user, post: post, score: score})
-        case Repo.insert(changeset) do
-          {:ok, vote} ->
-            conn
-            |> put_status(:created)
-            |> render("show.json", vote: vote)
-          {:error, changeset} ->
-            conn
-            |> put_status(:unprocessable_entity)
-            |> render(Api.ChangesetView, "error.json", changeset: changeset)
-        end
-      else 
-          conn
-          |> put_status(:unprocessable_entity)
-          |> json(%{error: "Post unavailable"}) 
-      end
-    else 
-      new_vote = Vote.update_changeset(cur_vote, %{score: score})
-      case Repo.update(new_vote) do
-          {:ok, vote} ->
-            conn
-            |> put_status(:created)
-            |> render("show.json", vote: vote)
-          {:error, changeset} ->
-            conn
-            |> put_status(:unprocessable_entity)
-            |> render(Api.ChangesetView, "error.json", changeset: changeset)
+    case Repo.get(Post, post_id) do
+      nil -> 
+        conn
+        |> put_status(:unprocessable_entity)
+        |> json(%{error: "Post unavailable"}) 
+      post ->
+        cur_vote = from(v in Vote, where: v.user_id == ^(user.id) and v.post_id == ^post_id) |> Repo.one
+        case cur_vote do
+          nil -> 
+            changeset = Vote.changeset(%Vote{}, %{user: user, post: post, score: score})
+            case Repo.insert(changeset) do
+              {:ok, vote} ->
+                conn
+                |> put_status(:created)
+                |> render("show.json", vote: vote)
+              {:error, changeset} ->
+                conn
+                |> put_status(:unprocessable_entity)
+                |> render(Api.ChangesetView, "error.json", changeset: changeset)
+            end
+          _ ->
+            new_vote = Vote.update_changeset(cur_vote, %{score: score})
+            case Repo.update(new_vote) do
+              {:ok, vote} ->
+                conn
+                |> put_status(:created)
+                |> render("show.json", vote: vote)
+              {:error, changeset} ->
+                conn
+                |> put_status(:unprocessable_entity)
+                |> render(Api.ChangesetView, "error.json", changeset: changeset)
+            end
         end
     end
   end
