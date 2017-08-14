e9efc89d8eed79840286f05ee5e4daf7356f74be
Colby Cox <coxjc@seas.upenn.edu>
2017-08-13 21:50:11 -0400

    SUB: Repeat flags MSG: Check if a flag already exists for a post before creating another

--- web/controllers/flag_controller.ex
+++ web/controllers/flag_controller.ex
@@ -13,37 +13,45 @@ defmodule Api.FlagController do
 
   def create(conn, %{"post_id" => post_id, "reason" => reason}) do
     flagger_id = conn.assigns.user.id
-    case Repo.get(User, flagger_id) do
+    cur_flag = Repo.get_by(Flag, post_id: post_id)
+    #check if a flag already exists for this post
+    case cur_flag do
       nil ->
-        conn
-        |> put_status(:unprocessable_entity)
-        |> json(%{error: "User not found"})
-      user ->
-        case Repo.get(Post, post_id) do
+        case Repo.get(User, flagger_id) do
           nil ->
             conn
             |> put_status(:unprocessable_entity)
             |> json(%{error: "User not found"})
-          post ->
-            # create a flag with the flagging user, the post, and why it's being flagged
-            post_changeset = Flag.changeset(%Flag{}, %{flagger: conn.assigns.user, post: post, reason: reason})
-            case Repo.insert(post_changeset) do
-              {:ok, flag} ->
-                Task.async(Twilio, :flag_notify, [flag.id])
-                conn
-                |> json(%{message: "Reported"})
-              {:error, error} ->
+          user ->
+            case Repo.get(Post, post_id) do
+              nil ->
                 conn
                 |> put_status(:unprocessable_entity)
-                |> render(Api.ChangesetView, "error.json", changeset: error)
+                |> json(%{error: "User not found"})
+              post ->
+                # create a flag with the flagging user, the post, and why it's being flagged
+                post_changeset = Flag.changeset(%Flag{}, %{flagger: conn.assigns.user, post: post, reason: reason})
+                case Repo.insert(post_changeset) do
+                  {:ok, flag} ->
+                    Task.async(Twilio, :flag_notify, [flag.id])
+                    conn
+                    |> json(%{message: "Reported"})
+                  {:error, error} ->
+                    conn
+                    |> put_status(:unprocessable_entity)
+                    |> render(Api.ChangesetView, "error.json", changeset: error)
+                end
             end
         end
+      flag ->
+        conn
+        |> json(%{message: "Reported"})
     end
   end
 
   def show(conn, %{"id" => id}) do
     case Repo.get(Flag, id) do
-      nil ->  
+      nil ->
         conn |> put_status(:unprocessable_entity) |> json(%{message: "Flag not found"})
       flag ->
         render(conn, "show.json", flag: flag)
@@ -52,7 +60,7 @@ defmodule Api.FlagController do
 
   def update(conn, %{"id" => id, "active" => active}) do
     case Repo.get(Flag, id) do
-      nil -> 
+      nil ->
         conn |> put_status(:unprocessable_entity) |> json(%{message: "Flag not found"})
       flag ->
         changeset = Flag.update_flag_changeset(flag, %{active: active})
