c3d9634d050f60297bf804303444eabf941562f8
Colby Cox <coxjc@seas.upenn.edu>
2017-08-28 23:39:35 -0400

    SUB: Vote Controller Fix MSG: Fix Vote Controller in order to change a Post's score based on difference between old Vote score and new. Add clarifying comments.

--- web/controllers/vote_controller.ex
+++ web/controllers/vote_controller.ex
@@ -8,42 +8,43 @@ defmodule Api.VoteController do
 
   def vote(conn, %{"post_id" => post_id, "score" => score}) do
     user = conn.assigns.user
+    # see if the post exists
     case Repo.get(Post, post_id) do
+      # if the post does not exist, abort
       nil -> 
         conn
         |> put_status(:unprocessable_entity)
         |> json(%{error: "Post unavailable"}) 
+      # call the found post `post`
       post ->
+        # find a vote for the user by the calling user
         case (from(v in Vote, where: v.user_id == ^(user.id) and v.post_id == ^post_id) |> Repo.one) do
+          # if the user has not voted on the post
           nil -> 
             changeset = Vote.changeset(%Vote{}, %{user: user, post: post, score: score})
-            # update post's score based on past post score and user's vote
-            post_changeset = Post.update_score_changeset(post, %{score: (post.score + score)})
             case Repo.insert(changeset) do
               {:ok, vote} ->
-                upd_post = Repo.update!(post_changeset)
-                IO.inspect upd_post
+                # update post's score based on past post score and user's vote
+                upd_post = Post.update_score_changeset(post, %{score: post.score + vote.score}) |> Repo.update!
                 conn
                 |> put_status(:created)
-                |> render("show.json", vote: vote)
                 |> json(%{vote: vote.score, score: upd_post.score})
               {:error, changeset} ->
                 conn
                 |> put_status(:unprocessable_entity)
                 |> render(Api.ChangesetView, "error.json", changeset: changeset)
             end
+          # if the user has already voted
           cur_vote ->
-            new_vote = Vote.update_changeset(cur_vote, %{score: score})
-            case Repo.update(new_vote) do
+            # calculate the new score of the vote based on the difference between the old vote's score and the new
+            new_score = score - cur_vote.score 
+            case Repo.update(Vote.update_changeset(cur_vote, %{score: score})) do
               {:ok, vote} ->
-                if cur_vote.score != score do
-                  # update post's score based on past post score and user's vote
-                  post_changeset = Post.update_score_changeset(post, %{score: (post.score + score)})
-                  Repo.update!(post_changeset)
-                end
+                # update post's score based on past post score and user's vote
+                upd_post = Post.update_score_changeset(post, %{score: (post.score + new_score)}) |> Repo.update!
                 conn
                 |> put_status(:created)
-                |> render("show.json", vote: vote)
+                |> json(%{vote: vote.score, score: upd_post.score})
               {:error, changeset} ->
                 conn
                 |> put_status(:unprocessable_entity)
