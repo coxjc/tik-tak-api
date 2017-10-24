4539bdc377c21499dda016f7cf8d8b782c9b1793
Colby Cox <coxjc@seas.upenn.edu>
2017-10-24 18:06:18 -0400

    Add 100 post limit for new and hot posts

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -119,7 +119,7 @@ defmodule Api.PostController do
 ############################################
 
   defp new_post_ids(lat, lng, distance, max) do
-    case Ecto.Adapters.SQL.query(Repo, "SELECT id, ( 3959 * acos ( cos ( radians(?) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(?) ) + sin ( radians(?) ) * sin( radians( lat ) ) ) ) AS distance FROM post WHERE is_comment = 0 HAVING distance < ? ORDER BY inserted_at DESC", [lat, lng, lat, distance]) do
+    case Ecto.Adapters.SQL.query(Repo, "SELECT id, ( 3959 * acos ( cos ( radians(?) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(?) ) + sin ( radians(?) ) * sin( radians( lat ) ) ) ) AS distance FROM post WHERE is_comment = 0 HAVING distance < ? ORDER BY inserted_at DESC LIMIT 100", [lat, lng, lat, distance]) do
       {:ok, %Mariaex.Result{rows: rows}} ->
         rows
       error ->
@@ -130,7 +130,7 @@ defmodule Api.PostController do
   #https://medium.com/hacking-and-gonzo/how-hacker-news-ranking-algorithm-works-1d9b0cf2c08d
   defp hot_post_ids(lat, lng, distance, gravity, max) do
     {{_, _, _}, {hour, _, _}} = :os.timestamp |> :calendar.now_to_datetime
-    case Ecto.Adapters.SQL.query(Repo, "SELECT id, ( 3959 * acos ( cos ( radians(?) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(?) ) + sin ( radians(?) ) * sin( radians( lat ) ) ) ) AS distance, (score / (POWER(( SELECT (UNIX_TIMESTAMP(UTC_TIMESTAMP())-UNIX_TIMESTAMP(inserted_at)) / 3600) + 2, ?))) as rating FROM post WHERE is_comment = 0 HAVING distance < ? ORDER BY rating DESC", [lat, lng, lat, gravity, distance]) do
+    case Ecto.Adapters.SQL.query(Repo, "SELECT id, ( 3959 * acos ( cos ( radians(?) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(?) ) + sin ( radians(?) ) * sin( radians( lat ) ) ) ) AS distance, (score / (POWER(( SELECT (UNIX_TIMESTAMP(UTC_TIMESTAMP())-UNIX_TIMESTAMP(inserted_at)) / 3600) + 2, ?))) as rating FROM post WHERE is_comment = 0 HAVING distance < ? ORDER BY rating DESC LIMIT 100", [lat, lng, lat, gravity, distance]) do
       {:ok, %Mariaex.Result{rows: rows}} ->
         rows
       error ->
